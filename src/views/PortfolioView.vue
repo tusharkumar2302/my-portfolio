704250b57ef38e557629addb2dece529369bdf8c
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-06-24 03:57:15 +0530

    removed github icons

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -151,7 +151,7 @@ export default {
                   </div>
                   <div class="w-full flex justify-end">
                     <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'" :href="item.github" target="_blank" rel="noreferrer"
+                      <!-- <a v-if="item.github !== 'null'" :href="item.github" target="_blank" rel="noreferrer"
                         title="View github repository" class="transition-all hover:text-accent">
                         <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
                           stroke-linecap="round" stroke-linejoin="round" height="16" width="16"
@@ -159,8 +159,8 @@ export default {
                           <path
                             d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                           </path>
-                        </svg></a>
-                      <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
+                        </svg></a> -->
+                      <!-- <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
                         title="View finished project" class="transition-all hover:text-accent">
                         <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
                           stroke-linecap="round" stroke-linejoin="round" height="18" width="18"
@@ -168,7 +168,7 @@ export default {
                           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
                           <line x1="10" y1="14" x2="21" y2="3"></line>
-                        </svg></a>
+                        </svg></a> -->
                     </div>
                   </div>
                 </div>
